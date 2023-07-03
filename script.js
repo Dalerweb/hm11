9e495d8ee9b4cb2c944f15975e0b93e1ca50665d
Dalerweb <dalerjurakulov7777@gmail.com>
2023-07-03 17:31:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let arr = ['amir', 'zokir', 'ravshan', 'jamshud', 'alex']
+
+let evenNames = arr.filter((name)=> name.length % 2 === 0) 
+let oddNames = arr.filter((name)=> name.length % 2 === 1)
+
+console.log (evenNames,oddNames)
+
+
